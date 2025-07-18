c34e1c224cf65082de0816253c9a63002a6e0a77
Rohan Bairagi <143432752+ROHANBAIRAGI123@users.noreply.github.com>
2025-07-18 18:31:23 +0530

    feat: add fonts for the components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import localFont from "next/font/local";
+
+const outfit = localFont({
+  src: "../fonts/Outfit/Outfit-VariableFont_wght.ttf",
+  variable: "--font-outfit",
+});
+
+const inter = localFont({
+  src: "../fonts/Inter/Inter-VariableFont_opsz,wght.ttf",
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Book Tracker App",
@@ -12,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${outfit.variable} ${inter.variable}`}>
       <body className={` antialiased`}>{children}</body>
     </html>
   );
